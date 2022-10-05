c1e8c25189a059ba95fbc810cc6dcc9a3afe0a92
Mark <kalmark@mail.ru>
2022-10-05 22:54:32 +0300

    Add Ex_010_MethodArray

---
+++ Ex_010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 21, 2, 43, 4, 5, 66, 7, 43 };
+int n = array.Length;
+Console.Write("Введите число: ");
+int find = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
