3284ddb4501be591b984cd1af751ea2ba6d9daf3
Mark <kalmark@mail.ru>
2022-10-05 22:37:48 +0300

    Add Ex_009_IntroArray

---
+++ Ex_009_IntroArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = { 11, 23, 34, 104, 104, 1555, 17, 83, 99 };
+int max = array[0];
+for (int i = 0; i <=8; i++)
+{
+    if (array[i] > max) max = array[i];
+}
+
+Console.Write(max + " - максимальное число из ряда чисел: ");
+for (int i = 0; i <=8; i++)
+{
+    Console.Write(array[i] + " ");
+}
