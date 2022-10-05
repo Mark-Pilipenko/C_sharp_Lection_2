84b3119be93ce6f27e3b24560a6cb3a83d098a94
Mark <kalmark@mail.ru>
2022-10-05 22:06:17 +0300

    Add procedure

--- Ex_008_Max_9/Program.cs
+++ Ex_008_Max_9/Program.cs
@@ -1,19 +1,24 @@
-﻿int a1 = 15;
-int a2 = 21;
-int a3 = 39;
-int a4 = 12;
-int a5 = 23;
-int a6 = 33;
-int a7 = 13;
-int a8 = 23;
-int a9 = 333;
-int max = a1;
-if(a2 > max) max = a2;
-if(a3 > max) max = a3;
-if(a4 > max) max = a4;
-if(a5 > max) max = a5;
-if(a6 > max) max = a6;
-if(a7 > max) max = a7;
-if(a8 > max) max = a8;
-if(a9 > max) max = a9;
-Console.WriteLine(max + " - максимальное число из ряда чисел: " + a1 + " " + a2 + " " + a3 + " " + a4 + " " + a5 + " " + a6 + " " + a7 + " " + a8 + " " + a9 );
+﻿int Max(int Arg1, int Arg2, int Arg3)
+{
+    int result = Arg1;
+    if(Arg2 > result) result = Arg2;
+    if(Arg3 > result) result = Arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 333;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max + " - максимальное число из ряда чисел: " + a1 + " " + b1 + " " + c1 + " " + a2 + " " + b2 + " " + c2 + " " + a3 + " " + b3 + " " + c3 );
